eb719aab3ebf27e45f6d98efe06eef109e7815f1
eduardo-raniero <eduardoraniero@gmail.com>
2022-02-17 10:29:53 -0300

    home

--- src/screens/homeLogin/homeLogin.jsx
+++ src/screens/homeLogin/homeLogin.jsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
+
+import './homeLogin.scss';
+
+import { BsGoogle } from 'react-icons/bs';
+
 export default function HomeLogin(){
     return(
-        <>
-            <h1>Hello there</h1>
-        </>
+        <div className="aligner">
+            <div className="container">
+                <h1>MoneyMonkey</h1>
+                <div className='formBox'>
+                    <Link to="/account"><BsGoogle /> Login com Google</Link>
+                </div>
+                <br />
+                <small>Powered by: <a href="https://github.com/eduardo-raniero">E.R.S</a> </small>
+            </div>
+        </div>
     )
 }
